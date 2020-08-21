669a461f414c599931a5734636892a32917734f9
Juan Basalo <jbasalo@jmm-solutions.com>
2020-08-20 21:33:49 -0500

    migration

--- PortfolioManager/IBKRApi/Startup.cs
+++ PortfolioManager/IBKRApi/Startup.cs
@@ -2,11 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using AutoMapper;
-using Common.Auth;
-using Common.Commands;
-using Common.RabbitMq;
-using Common.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -14,11 +9,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using PortfolioSystem.Data_Access.SqlServer;
-using PortfolioSystem.Handlers;
-using PortfolioSystem.Helpers;
 
-namespace PortfolioSystem
+namespace IBKRApi
 {
     public class Startup
     {
@@ -33,14 +25,6 @@ namespace PortfolioSystem
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddRabbitMq(Configuration);
-            services.AddCustomDbContext<PortfolioSystemContext>(Configuration);
-            services.AddCustomScopedServices();
-            services.AddScoped(typeof(ISqlServerRepository<>), typeof(SqlServerRepository<>));
-            //services.AddCustomAuthentication(Configuration);
-            services.AddScoped<ICommandHandler<CreatePortfolio>, CreatePortfolioHandler>()
-                    .AddScoped<ICommandHandler<BuySecurity>, BuySecuritiesHandler>();
-            services.AddAutoMapper(typeof(Startup));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,12 +34,10 @@ namespace PortfolioSystem
             {
                 app.UseDeveloperExceptionPage();
             }
-            app.UseCors("default");
 
             app.UseRouting();
 
-            //app.UseAuthentication();
-            //app.UseAuthorization();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
